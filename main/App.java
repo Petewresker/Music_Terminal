ed6ea1e9cca1271196533994e2cb9d623641bd6f
unknown <minecopper.55@gmail.com>
2025-02-11 22:26:30 +0700

    Full Woking but need a stop button

--- main/App.java
+++ main/App.java
@@ -13,9 +13,9 @@ public class App {
     }
 
     private void addSong(){
-        songManager.addSongToPlaylist("", "Better When I'm Dancin", "Megan");
-        songManager.addSongToPlaylist("", "SkyLab Plan", "Daniel Pimberton");
-        songManager.addSongToPlaylist("", "Bird of the feather", "Billie Eilish");
+        songManager.addSongToPlaylist("resource/Music.wav", "Time & Life", "Mitty");
+        songManager.addSongToPlaylist("resource/Music.wav", "SkyLab Plan", "Daniel Pimberton");
+        songManager.addSongToPlaylist("resource/Music.wav", "stay", "Interstallar");
     }
     
 
@@ -23,10 +23,22 @@ public class App {
         Scanner scanner = new Scanner(System.in);
 
         while(true){
-            System.out.println("Japotify Inc.");
-            System.out.println("-------------");
+            System.out.println("--------------------------------------------------------------------");
+            System.out.println("   ___                                     _____             ");
+            System.out.println("  |_  |                                   |_   _|            ");
+            System.out.println("    | | __ _ _ __ ___   ___  _ __  _   _    | | _ __   ___   ");
+            System.out.println("    | |/ _` | '_ ` _ \\ / _ \\| '_ \\| | | |   | || '_ \\ / __|  ");
+            System.out.println("/\\__/ / (_| | | | | | | (_) | | | | |_| |  _| || | | | (__ _ ");
+            System.out.println("\\____/ \\__,_|_| |_| |_|\\___/|_| |_|\\__, |  \\___/_| |_|\\___(_)");
+            System.out.println("                                    __/ |                    ");
+            System.out.println("                                   |___/                     ");
+
+            System.out.println("");
+            System.out.println("                         Your's heart are just beatbox of your LIFE.");
+            System.out.println("--------------------------------------------------------------------");
             System.out.println("1. Play a song");
             System.out.println("2. Closed app");
+            System.out.println("3. Instruction Manaul");
 
             System.out.print("Which one do you prefer : ");
             Choice = scanner.nextInt();
--- main/SongManager.java
+++ main/SongManager.java
@@ -1,13 +1,16 @@
 package main;
-import java.io.File;
 import java.util.Scanner;
+import java.io.File;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 
 public class SongManager {
     
     private PlayList playList;
+    private Clip clip;
 
     public SongManager(){
         this.playList = new PlayList();
@@ -31,6 +34,15 @@ public class SongManager {
     System.out.println("");
     System.out.print("What song you want to Listen : ");
     int songIndex = scanner.nextInt();
+
+    if(songIndex >= 1 && songIndex < playList.getSize()){
+        MusicFile song = playList.getSong(songIndex - 1);
+        System.out.println("Now Playing "+song.getTitle() +" By "+ song.getArtist());
+        JOptionPane.showMessageDialog(null, "Press ok to stop.");
+
+    }else{
+        clearScreen();
+    }
     
     //Playing a song by media.
 }
@@ -43,11 +55,21 @@ public class SongManager {
 
     //Import from youtube -_-
     private void playsong(String filename){
-        String path = getClass().getResource(filename).getPath();
-        Media media = new Media(new File(path).toURI().toString());
-        MediaPlayer mediaPlayer = new MediaPlayer(media);
-        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-        mediaPlayer.play();
+        //Song implement 
+        try{
+            File musicePath = new File(filename);
+
+            if(musicePath.exists()){
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicePath);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInput);
+                clip.start();
+            }else{
+                System.out.println("File isn't exist.");
+            }
+        }catch(Exception e){
+            System.out.println(e);
+        }
     }
 
 }
